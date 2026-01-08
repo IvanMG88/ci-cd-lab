74108fa86a9fbe97fc0d79948ba21d6ad73d7b91
Jesus Ivan Martinez Gonzalez <mgji_18@hotmail.com>
2026-01-08 23:01:01 +0000

    set correct account_tier for storage account

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "storage" {
   name = "contiuosdeployment"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
-  account_tier = "Standar"
+  account_tier = "Standard"
   account_replication_type = "LRS"
 }
 
