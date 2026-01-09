a9a456123cfb71ab472cf5ef38a3ee76c2764dbf
Jesus Ivan Martinez Gonzalez <mgji_18@hotmail.com>
2026-01-09 17:27:34 +0000

    set correct name for storage aacount backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "azurerm" {
-      storage_account_name = "estadosterraform"
+      storage_account_name = "estadosterraformjimg"
       container_name = "githubactionstate"  
       key = "estados.tfstate"
     }
